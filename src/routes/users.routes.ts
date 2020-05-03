4d6e1e0aec54ad239744f08c60865b9b86a0cfc7
Guilherme Matheus Corrêa <guilherme.matheus.correa@gmail.com>
2020-05-03 12:22:10 -0300

    feat: Upload avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (request, response) => {
   try {
@@ -18,5 +23,27 @@ userRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+userRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatar_filename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
+  },
+);
 
 export default userRouter;
